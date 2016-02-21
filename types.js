da1173f647d50b2f63901eadac3df842e6862e9c
Richie Manis <richie.manis@gmail.com>
2016-02-21 15:30:34 -0500

    Add Vector class.

--- types.js
+++ types.js
@@ -19,6 +19,53 @@ function makeGuid() {
         S4() + S4() + S4()).toLowerCase();
 }
 
-function vector(x, y) {
-    return { x: x, y: y };
+var Vector = function(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
 }
+
+Vector.prototype.normalize = function() {
+    var len = this.length();
+    return new Vector(this.x / len, this.y / len);
+}
+
+Vector.prototype.length = function() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+}
+
+Vector.prototype.lengthSquared = function() {
+    return this.x * this.x + this.y * this.y;
+}
+
+Vector.prototype.subtract = function(b) {
+    return new Vector(this.x - b.x, this.y - b.y);
+}
+
+Vector.prototype.scale = function(s) {
+    return new Vector(this.x * s, this.y * s);
+}
+
+Vector.prototype.dot = function(v) {
+    return this.x * v.x + this.y * v.y;
+}
+
+// Project this onto vector v
+// a * ((a . b) / (|a| ** 2))
+Vector.prototype.project = function(v) {
+    var unit = v.normalize();
+    var scaling = this.dot(v);
+    return unit.scale(scaling);
+}
+
+// v.dot(w) = v.length * w.length * cos(angle)
+Vector.prototype.angleTo = function(v) {
+    var num = this.dot(v) / (this.length() * v.length())
+    return Math.acos(num);
+}
+
+function vector(x, y) {
+    return new Vector(x, y);
+}
+
+// Vector that represents a global heading of zero
+var RIGHT_VEC = new Vector(1, 0);
