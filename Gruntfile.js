39f9f503b030ac8be2e67872601ecead5794fa68
Richie Manis <richie.manis@gmail.com>
2016-02-28 08:54:13 -0500

    Configure grunt to deploy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,40 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg : grunt.file.readJSON('package.json'),
+
+        copy : {
+            main : {
+                src : 'src/index.html',
+                dest : 'build/index.html'
+            }
+        },
+
         jshint : {
             all : ['Gruntfile.js', 'src/**/*.js']
         },
+
         uglify : {
             options : {
             },
-            build : {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+            common : {
+                src : 'src/common/**/*.js',
+                dest: 'build/common.min.js'
+            },
+            client : {
+                src : 'src/client/**/*.js',
+                dest : 'build/client.min.js'
+            },
+            server : {
+                src : 'src/server/**/*.js',
+                dest: 'build/server.min.js'
             }
         }
+
     });
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'uglify', 'copy']);
 };
