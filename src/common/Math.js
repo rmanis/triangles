55de9b3fb7a0d139587ee6f71fe7493129b4bec1
Richie Manis <richie.manis@gmail.com>
2017-09-03 22:22:15 -0400

    Add Math.

---
+++ src/common/Math.js
@@ -0,0 +1,17 @@
+
+define([
+], function() {
+    var math = {
+        TWO_PI : Math.PI * 2,
+
+        PI : Math.PI,
+
+        randomInt : function(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+    };
+
+    return math;
+});
