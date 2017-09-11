58205a4723a46ffb83f77f635e4089a1175875d9
Richie Manis <richie.manis@gmail.com>
2017-09-11 03:29:22 -0400

    Add planet creation and editing.

--- src/editor/planets/PropertyInputs.js
+++ src/editor/planets/PropertyInputs.js
@@ -1,6 +1,11 @@
 
 define([
-], function() {
+    'common/Constants',
+    'common/Guid',
+    'common/types/Coordinate',
+    'common/types/Planet',
+    'common/types/Vector',
+], function(Constants, Guid, Coordinate, Planet, Vector) {
     var PropertyInputs = function(element, planets, sectorView) {
         this.element = element;
         this.planets = planets;
@@ -21,6 +26,14 @@ define([
         this.yInput      = document.getElementById("yInput");
         this.radiusInput = document.getElementById("radiusInput");
         this.massInput   = document.getElementById("massInput");
+
+        this.newPlanetButton = document.getElementById("newPlanet");
+        this.delPlanetButton = document.getElementById("deletePlanet");
+        this.commitPlanetButton = document.getElementById("commitPlanet");
+
+        this.newPlanetButton.addEventListener("click", this.newPlanet.bind(this));
+        this.delPlanetButton.addEventListener("click", this.deleteSelectedPlanet.bind(this));
+        this.commitPlanetButton.addEventListener("click", this.commitSelectedPlanet.bind(this));
     };
 
     PropertyInputs.prototype.planetSelected = function(id) {
@@ -49,6 +62,45 @@ define([
             this.radiusInput.value = "";
             this.massInput.value = "";
         }
+    };
+
+    PropertyInputs.prototype.pullForm = function() {
+        var id = this.sectorView.selectedPlanetId;
+        if (!id) {
+            return;
+        }
+
+        var p = this.planets.planets[id];
+        if (p) {
+            p.id = this.idInput.value;
+            p.coord.pos.x = Number.parseInt(this.xInput.value);
+            p.coord.pos.y = Number.parseInt(this.yInput.value);
+            p.coord.radius = Number.parseInt(this.radiusInput.value);
+            p.coord.radius = Number.parseInt(this.massInput.value);
+            this.planets.addPlanet(p);
+        }
+    };
+
+    PropertyInputs.prototype.newPlanet = function(e) {
+        this.planetSelected(null);
+
+        var half = Constants.sectorSize / 2;
+        var p = new Planet(
+            Guid(),
+            new Coordinate(this.sectorView.sector,
+                new Vector(half, half)),
+            50,
+            50);
+        this.planets.addPlanet(p);
+
+        this.sectorView.selectPlanet(p.id);
+    };
+
+    PropertyInputs.prototype.deleteSelectedPlanet = function(e) {
+    };
+
+    PropertyInputs.prototype.commitSelectedPlanet = function(e) {
+        this.pullForm();
     };
 
     return PropertyInputs;
